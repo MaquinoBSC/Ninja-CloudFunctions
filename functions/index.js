ae6d182f5b949734f2a8af088044eb8fdd150a57
MaquinoBSC <martinare.ipn@gmail.com>
2021-08-19 14:41:22 -0500

    crear funciiones de tipo endpoint request

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,24 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// Http request 1
+//endpoint request kind of cloud functions
+
+
+//exports es usado porque vamos a exportar esta fincion
+//randonNumber es el nombre de la funcion
+//functions es la importacion de arriba
+//https es el tipo de funcion que estamos creando
+//onRequest es porque estamos creando un endpoint request
+//request tiene la informacion sobre la peticion
+//reponse es la respuesta que se manda al cliente
+exports.randonNumber= functions.https.onRequest((request, response)=> {
+    const number= Math.round(Math.random() * 100);
+    console.log(number);
+    console.log("martin");
+    response.send(number.toString());
+});
+
+//Http request 2
+exports.toTheDojo= functions.https.onRequest((request, response)=> {
+    response.redirect('https://www.thenetninja.co.uk');
+});
