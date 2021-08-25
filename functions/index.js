2f8fa740ac8929219c9305b2bb4196ecb4a5d5f0
MaquinoBSC <martinare.ipn@gmail.com>
2021-08-24 19:14:38 -0500

    Crear registro de usuarios con authentication trigger function

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,6 @@
 const functions = require("firebase-functions");
+const admin= require("firebase-admin"); //El SDK de admin es necesario para poder interactuar con firestore database dentro de nuestras cloud functions
+admin.initializeApp();
 
 // Http request 1
 //endpoint request kind of cloud functions
@@ -52,18 +54,25 @@ exports.calculator= functions.https.onCall((data, context)=> {
 
 
 //auth trigger (new user signup)
+//Con los triggers no retornamos nada al cliente porque no se esta haciendo peticion de nada
 
 //Esta sera una authentication trigger function, ya no es una funcion https
 //esto esta basado en cuando pasa algo con el usuario
 //cuando un usuario sea creado se dispara el callback
 //ese callback lleva como parametro el usuario creado
+//para los backgroud  triggers se debe retornar un valor o promesa
 exports.newUserSignup= functions.auth.user().onCreate((user)=> {
-    console.log("user ", user);
-    console.log("user created ", user.email, user.uid);
+    //Si la coleccion o el documento no existen, firebase lo crea por nosotros
+    return admin.firestore().collection('users').doc(user.uid).set({
+        email: user.email,
+        upVotedOn: []
+    });
 });
 
 //auth trigger (user deleted)
 exports.userDeleted= functions.auth.user().onDelete((user)=> {
-    console.log("user ", user);
-    console.log("user deleted ", user.email, user.uid);
+    //Obtenemos la referencia del documento
+    const doc= admin.firestore().collection('users').doc(user.uid);
+    //Eliminamos el documento que corresponde al usuario y retornamos una promesa
+    return doc.delete();
 });
