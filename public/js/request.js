91b6e662e2ab855bb3fd71689a27c8f0d9a1560b
MaquinoBSC <martinare.ipn@gmail.com>
2021-08-26 11:56:07 -0500

    Agregar instancia de Vue

--- public/js/request.js
+++ public/js/request.js
@@ -1,19 +1,22 @@
-const ref= firebase.firestore().collection('requests'); //Obetner la referencia a la coleccion requests
+var app = new Vue({//Instancia de Vue
+    el: '#app',
+    data: {
+        requests: []
+    },
+    mounted() {
+        const ref= firebase.firestore().collection('requests'); //Obetner la referencia a la coleccion requests
+        
+        //onSnapshot es una funcion que ejecuta un callback cada que ocurre un cambio en la coleccion
+        //snapshot representa la coleccion con los datos ya actualizados
+        ref.onSnapshot((snapshot)=> {
+            let requests= [];
+        
+            snapshot.forEach((doc)=> {
+                requests.push({...doc.data(), id: doc.id});
+            });
+            this.requests= requests;
+        });
+    },
+})
 
 
-//onSnapshot es una funcion que ejecuta un callback cada que ocurre un cambio en la coleccion
-//snapshot representa la coleccion con los datos ya actualizados
-ref.onSnapshot((snapshot)=> {
-    let requests= [];
-
-    snapshot.forEach((doc)=> {
-        requests.push({...doc.data(), id: doc.id});
-    });
-
-    let html= ``;
-    requests.forEach((request)=> {
-        html += `<li>${request.text}</li>`;
-    });
-
-    document.querySelector('ul').innerHTML= html;
-});
